3ce632f6d6db75d6199e48ca5df6618a262c60c6
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-23 19:07:38 +0500

    create Menuitem model

--- app_foods/models.py
+++ app_foods/models.py
@@ -18,3 +18,18 @@ class User(AbstractUser):
     @property
     def is_admin(self):
         return self.role == 'admin'
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at']

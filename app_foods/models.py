1c0457f186616fb61174081ea214c020d66828dd
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-23 19:42:26 +0500

    create prder item model

--- app_foods/models.py
+++ app_foods/models.py
@@ -45,3 +45,12 @@ class OrderModel(models.Model):
         preparation_time = 5 * (self.items.count() // 4 + 1)
         delivery_time = self.distance_km * 3
         return preparation_time + delivery_time
+
+
+class OrderItemModel(models.Model):
+    order = models.ForeignKey(OrderModel, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItemModel, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.menu_item.name} x {self.quantity}"

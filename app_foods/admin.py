c35cc3965875b607a3f8dd48cc874e45056d021b
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-23 19:44:44 +0500

    register admin panel all model

--- app_foods/admin.py
+++ app_foods/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserModel, MenuItemModel, OrderModel, OrderItemModel
 
-# Register your models here.
+admin.site.register(UserModel)
+admin.site.register(MenuItemModel)
+admin.site.register(OrderModel)
+admin.site.register(OrderItemModel)

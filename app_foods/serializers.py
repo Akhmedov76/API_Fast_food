7ab44f7d9655093687dd44e208c6e8d57b7e053d
Akhmedov76 <ahmedovj7686@gmail.com>
2025-01-23 22:06:32 +0500

    add views

--- app_foods/serializers.py
+++ app_foods/serializers.py
@@ -1,19 +1,27 @@
 from rest_framework import serializers
 from .models import UserModel
 
+UserChoice = [
+    'admin', 'user', 'courier'
+]
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
     confirm_password = serializers.CharField(min_length=8, write_only=True)
     username = serializers.CharField(max_length=150, required=True)
-    role = serializers.ChoiceField(default='user', required=False)
+    role = serializers.ChoiceField(
+        default=UserChoice[0],
+        choices=UserChoice)
 
     class Meta:
         model = UserModel
         fields = ['username', 'password', 'role', 'confirm_password']
 
     def validate(self, data):
-        if data['password'] != data['confirm_password']:
+        confirmed_password = data.pop('confirm_password')
+
+        if data['password'] != confirmed_password:
             raise serializers.ValidationError({'confirm_password': 'Passwords must match'})
         return data
 
--- app_foods/views.py
+++ app_foods/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 
-# Create your views here.
+from app_foods.models import UserModel
+from app_foods.serializers import RegisterSerializer, LoginSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = UserModel.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class LoginView(APIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return serializer.validated_data

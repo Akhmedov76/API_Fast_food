ea859a742bd62b0bd89e915732445b36eacb353f
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-02 18:16:56 +0500

    Add user info

--- users/admin.py
+++ users/admin.py
@@ -9,7 +9,7 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'role', 'phone', 'is_staff')
     list_filter = ('role', 'is_staff', 'is_superuser')
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
+        ('User info', {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'address')}),
         ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )

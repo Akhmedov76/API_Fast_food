8915aa05dde11287541e2a7bdc6e4bac1a448674
Akhmedov76 <ahmedovj7686@gmail.com>
2025-02-07 10:00:17 +0500

    Create assets, static, templates

--- conf/urls.py
+++ conf/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -12,6 +14,8 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # JWT Authentication
 urlpatterns += [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
